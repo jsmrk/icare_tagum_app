d41f7bdfbfa2f2ddb205bda2b7d373772557b075
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2023-12-01 23:00:19 +0800

    testing widgets

--- lib/screens/nickname_input.dart
+++ lib/screens/nickname_input.dart
@@ -19,14 +19,58 @@ class NicknameScreen extends StatelessWidget {
           Text(
             'What should we call you?',
             style: Theme.of(context).textTheme.titleMedium,
+            textAlign: TextAlign.left,
           ),
           //textbox
-          const TextField(),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 37),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.all(15),
+                prefixIcon: const Icon(
+                  Icons.person_outline_outlined,
+                  color: Colors.green,
+                  size: 27,
+                ),
+                hintText: 'Nickname',
+                hintStyle: const TextStyle(
+                    color: Colors.grey,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.green),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(
+                    color: Colors.green,
+                    width: 1.9,
+                  ),
+                ),
+              ),
+            ),
+          ),
           //button
-          ElevatedButton(
-            style: Theme.of(context).buttonTheme,
-            onPressed: () {},
-            child: const Text('Continue'),
+          GestureDetector(
+            onTap: () {},
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Container(
+                width: 151,
+                padding: const EdgeInsets.all(15),
+                color: Colors.green,
+                child: const Text(
+                  'Continue',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
           )
         ],
       ),
